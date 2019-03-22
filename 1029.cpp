7e6b1ec13c34278647fa4071b6e189dc01343c3c
resmony <munizresmony@live.com>
2019-03-22 12:26:26 -0300

    done

--- 1029.cpp
+++ 1029.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
+#include <tuple>
 
 using namespace std;
 
+tuple<int, int> fibonacci(int n, int &i) {
+    if (n < 2) return make_tuple(n, i);
+    return make_tuple(get<0>(fibonacci(n-1, ++i)) + get<0>(fibonacci(n-2, ++i)), i);
+}
+
 int main(int argc, char** argv) {
+    int nums, value, i;
+    cin >> nums;
+
+    for (auto j = 0; j < nums; j++) {
+        cin >> value;
+        auto out = fibonacci(value, i=0);
+        printf("fib(%d) =   %u  calls   =   %d\n", value, get<1>(out), get<0>(out));
+    }
 
     return EXIT_SUCCESS;
 }
